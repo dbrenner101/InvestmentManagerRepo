8a6010bfe82aeff2fed541141b6ea8c81cac05a2
Dave Brenner <dbrenner101@gmail.com>
2022-07-15 19:13:16 -0400

    Removed the exclusion for css and js

--- InvestmentManager/src/main/java/com/brenner/portfoliomgmt/security/WebSecurityConfig.java
+++ InvestmentManager/src/main/java/com/brenner/portfoliomgmt/security/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,11 +19,7 @@ public class WebSecurityConfig {
     @Autowired
     InvestmentUserDetailsService investmentUserDetailsService;
     
-    @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("*/css/**", "*/js/**");
-    }
-    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
