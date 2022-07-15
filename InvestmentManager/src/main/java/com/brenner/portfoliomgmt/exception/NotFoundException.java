2352373d5e3035b86f79d951dae489708ef160c0
Dave Brenner <dbrenner101@gmail.com>
2022-07-15 15:39:36 -0400

    Collapsed constructors to those used

--- InvestmentManager/src/main/java/com/brenner/portfoliomgmt/exception/NotFoundException.java
+++ InvestmentManager/src/main/java/com/brenner/portfoliomgmt/exception/NotFoundException.java
@@ -17,40 +17,10 @@ public class NotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 6004931991791288456L;
 
 	/**
-	 * 
-	 */
-	public NotFoundException() {}
-
-	/**
 	 * @param message
 	 */
 	public NotFoundException(String message) {
 		super(message);
-	}
-
-	/**
-	 * @param cause
-	 */
-	public NotFoundException(Throwable cause) {
-		super(cause);
-	}
-
-	/**
-	 * @param message
-	 * @param cause
-	 */
-	public NotFoundException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
 }
